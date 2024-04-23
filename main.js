0b108517a29dca6ac559b45dbad01f4b18cc2b6e
Sarim Rehan <166250973+sarimrehan12@users.noreply.github.com>
2024-04-23 23:56:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+// function egg (a:number , b:number){
+//     return a+b;
+// }
+// console.log(egg(7,9))
+// //name example
+// function greet(name: string): string{
+//     return `Hello , ${name}`
+// }
+// const greeting=greet("sarim rehan")
+// console.log(greeting);
+// //void funtion 
+function printHello() {
+    console.log("Hello");
+}
---
+++ main.ts
@@ -0,0 +1,15 @@
+
+
+// function egg (a:number , b:number){
+//     return a+b;
+// }
+// console.log(egg(7,9))
+// //name example
+// function greet(name: string): string{
+//     return `Hello , ${name}`
+// }
+// const greeting=greet("sarim rehan")
+// console.log(greeting);
+// //void funtion 
+
+}
